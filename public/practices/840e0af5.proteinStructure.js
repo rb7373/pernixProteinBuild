cfcbe379630c15055550f256aca4a298d96f680e
ruben abarca <abarca.ruben73@gmail.com>
2015-04-29 04:56:11 -0600

    with uglify, manual injecte file practices in index

--- public/practices/840e0af5.proteinStructure.js
+++ public/practices/840e0af5.proteinStructure.js
@@ -527,7 +527,7 @@ var Info = {
 	color: "0xFFFFFF",
 	addSelectionOptions: false,
 	use: "HTML5",
-	j2sPath: "components/JSmol/j2s",
+	j2sPath:"../JSmol/j2s",
 	isSigned: true,
 	script: "",
 	serverURL: "http://chemapps.stolaf.edu/jmol/jsmol/php/jsmol.php",
@@ -622,7 +622,7 @@ function LoadPDB()
 	$("#practice_session").append(Jmol.getAppletHtml("jmolApplet0", Info));
     $("#practice_session #jmolApplet0_appletinfotablediv").css("z-index", 0);
 
-	Jmol.script(jmolApplet0,'background IMAGE "components/images/background.jpg";');
+  Jmol.script(jmolApplet0,'background IMAGE "components/images/background.jpg";');
 	Jmol.script(jmolApplet0,'set specular 10; set specularPower 50; set specularExponent 2; label off; monitor off; set frank off; set ambient 90; set diffuse 90; measure OFF; set picking ON; selectionHalos OFF; hide all');
 	Jmol.script(jmolApplet0, molecules[currentMol].initTransform);
 
